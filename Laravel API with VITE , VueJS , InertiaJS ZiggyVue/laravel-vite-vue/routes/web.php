e1bb7153fc9f43123af18c23811612d985ec828e
xaibaly <xubair.jatoi@gmail.com>
2022-10-21 10:21:20 +0500

    3

---
+++ Laravel API with VITE , VueJS , InertiaJS ZiggyVue/laravel-vite-vue/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[ProductController::class, 'index'])->name('index');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::resource('products', ProductController::class);
